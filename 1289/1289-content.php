1378d269856000da952c1825b855c0e7aa8f6dfa
hoangnhata2 <33858680+hoangnhata2@users.noreply.github.com>
2019-09-26 12:19:05 +0700

    update

--- 1289/1289-content.php
+++ 1289/1289-content.php
@@ -7,18 +7,15 @@
       $url_path = $url_host . $matches[1][0];
       $url_path = str_replace('\\', '/', $url_path);
       ?>
-      <div class="container">
-        <div class="row">
-          <div class="type-1289">
+      <div class="type-1289">
+        <div class="container">
+          <div class="row"> 
             <div class="col-sm-3 col-sm-offset-4 frame">
               <ul></ul>
-              <div>
-                <div class="msj-rta macro">                        
+              <div>                                
                   <div class="text text-r" style="background:whitesmoke !important">
                     <input class="mytext" placeholder="Type a message"/>
                   </div> 
-
-                </div>
                 <div style="padding:10px;">
                   <span class="glyphicon glyphicon-share-alt"></span>
                 </div>                
